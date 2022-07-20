33676a5544e689e0007727fc695529d8d388e20c
hoipok1 <d2sambx@hotmail.com>
2022-07-19 18:17:51 -0700

    java.js

--- java.js
+++ java.js
@@ -1,39 +1,54 @@
 function computerPlay(){
     let computerMove = ["rock","paper","scissor"];
     const randomMove = Math.floor(Math.random()*3);
-
+    let computer = randomMove === 1 ? "rock":
+    randomMove === 2 ? "paper":
+    randomMove === 3 ? "scissor":{
+    }
     return computerMove[randomMove];
 }
-console.log(computerPlay());
 
 const computerSelection = computerPlay()
+console.log(computerSelection);
 const playerSelection = prompt("Choose rock,paper,or scissor");
 if (playerSelection){
     let playerOne = playerSelection.trim().toLowerCase();
     if( playerOne === "rock" || 
         playerOne === "paper" || 
-        playerOne === "scissor");
-       else{
-           alert("Your move is invalid")
+        playerOne === "scissor"){
+            
+        }
+        else{ 
+           alert("Your move is invalid");
        }
+       
     }       
 
-function playRound(playerSelection,computerSelection){
-    switch(playerSelection + computerSelection){
-        case (playerSelection ==="rock" && computerSelection ==="scissor"):
-        case (playerSelection ==="paper" && computerSelection ==="rock"):
-        case (playerSelection ==="scissor" && computerSelection ==="paper"):         
-            console.log("USER WINS")
-            break;
+function results(playerSelection,computerSelection){
         
-        case (playerSelection ==="rock" && computerSelection ==="paper"):
-        case (playerSelection ==="paper" && computerSelection ==="scissor"):
-        case (playerSelection ==="scissor" && computerSelection ==="rock"):
-            console.log("USER LOSES")
-            break;
+        if(playerSelection === computerSelection);{
+        result = ("IT'S A TIE")}
+    
+        if(playerSelection ==="rock" && computerSelection ==="scissor"){
+            result = ("USER WINS")
+        }
+        if(playerSelection ==="paper" && computerSelection ==="rock"){
+            result = ("USER WINS")
+        } 
+        if(playerSelection ==="scissor" && computerSelection ==="paper"){
+             result = ("USER WINS")}
         
-        case (playerSelection === computerSelection):
-            console.log("USER TIES")
-            break;    
+        
+        if(playerSelection ==="paper" && computerSelection ==="scissor"){
+                result = ("USER LOSES")
+            }
+        if(playerSelection ==="scissor" && computerSelection ==="rock"){
+                result = ("USER LOSES")
+            } 
+        if(playerSelection ==="rock" && computerSelection ==="paper"){
+                 result = ("USER LOSES")}
+     
+                return(result);
     }   
-};
+    
+console.log(results(playerSelection,computerSelection));
