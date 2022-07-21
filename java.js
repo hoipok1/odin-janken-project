7337d8df7fec49794eb06758ed8269938d4a7e28
hoipok1 <d2sambx@hotmail.com>
2022-07-20 20:49:23 -0700

    index.html

--- java.js
+++ java.js
@@ -1,54 +1,73 @@
+let userScore = 0;
+let computerScore = 0;
+let userChoice;
+let computerChoice;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const userChoice_span = document.getElementById("user-choice");
+const computerChoice_span = document.getElementById("computer-choice");
+
 function computerPlay(){
     let computerMove = ["rock","paper","scissor"];
     const randomMove = Math.floor(Math.random()*3);
-    let computer = randomMove === 1 ? "rock":
-    randomMove === 2 ? "paper":
-    randomMove === 3 ? "scissor":{
-    }
+    if (randomMove === 0){computerChoice = "rock"}
+    if (randomMove === 1){computerChoice = "paper"}
+    if (randomMove === 2){computerChoice = "scissor"}
+    computerChoice_span.innerHTML = computerChoice;
     return computerMove[randomMove];
 }
 
 const computerSelection = computerPlay()
 console.log(computerSelection);
 const playerSelection = prompt("Choose rock,paper,or scissor");
-if (playerSelection){
+
+function playerOne (playerSelection){
     let playerOne = playerSelection.trim().toLowerCase();
     if( playerOne === "rock" || 
         playerOne === "paper" || 
         playerOne === "scissor"){
-            
+        userChoice_span.innerHTML = userChoice = playerOne;   
+        return (playerOne);
         }
         else{ 
-           alert("Your move is invalid");
-       }
-       
-    }       
-
-function results(playerSelection,computerSelection){
-        
-        if(playerSelection === computerSelection);{
-        result = ("IT'S A TIE")}
-    
-        if(playerSelection ==="rock" && computerSelection ==="scissor"){
-            result = ("USER WINS")
+           alert("Your move is invalid")
         }
-        if(playerSelection ==="paper" && computerSelection ==="rock"){
-            result = ("USER WINS")
+    }       
+const playerMove = playerOne(playerSelection)
+
+function results(playerMove,computerSelection){
+        
+    if(playerMove === computerSelection){
+        alert("IT'S A TIE")} 
+        
+        
+        if(playerMove ==="rock" && computerSelection ==="scissor"){
+            alert("USER WINS");
+            userScore++
+        }
+        if(playerMove ==="paper" && computerSelection ==="rock"){
+            alert("USER WINS");
+            userScore++
         } 
-        if(playerSelection ==="scissor" && computerSelection ==="paper"){
-             result = ("USER WINS")}
+        if(playerMove ==="scissor" && computerSelection ==="paper"){
+            alert("USER WINS");
+            userScore++
+        }
         
-        
-        if(playerSelection ==="paper" && computerSelection ==="scissor"){
-                result = ("USER LOSES")
+
+        if(playerMove ==="paper" && computerSelection ==="scissor"){
+            alert("USER LOSES");
+                
             }
-        if(playerSelection ==="scissor" && computerSelection ==="rock"){
-                result = ("USER LOSES")
+        if(playerMove ==="scissor" && computerSelection ==="rock"){
+            alert("USER LOSES");
+                
             } 
-        if(playerSelection ==="rock" && computerSelection ==="paper"){
-                 result = ("USER LOSES")}
-     
-                return(result);
+        if(playerMove ==="rock" && computerSelection ==="paper"){
+            alert("USER LOSES");
+                 
+            }
     }   
     
-console.log(results(playerSelection,computerSelection));
+    console.log(results(playerMove,computerSelection));
+    
